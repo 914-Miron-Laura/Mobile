bdcd8854af2cd47d62d6eae2e3f31007bb12435f
914-Miron-Laura <miron_laura19911@yahoo.com>
2023-11-26 23:41:16 +0200

    teMa 2

--- DressUp/app/src/main/java/com/example/dressup/Item.kt
+++
@@ -1,3 +0,0 @@
-package com.example.dressup
-
-data class Item(val id:Int,val title:String,val description:String,val category:String,val price: String,val size:String,val color:String)
---
+++ lib/db_helper.dart
@@ -0,0 +1,70 @@
+import 'package:sqflite/sqflite.dart' as sql;
+
+class SQlHelper {
+  static Future<void> createTables(sql.Database database) async {
+    await database.execute("""CREATE TABLE data(
+      id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+      name TEXT,
+      category TEXT,
+      price TEXT,
+      size TEXT,
+      color TEXT
+    )""");
+  }
+
+  static Future<sql.Database> db() async {
+    return sql.openDatabase("database_name.db", version: 1,
+        onCreate: (sql.Database database, int version) async {
+      await createTables(database);
+    });
+  }
+
+  static Future<int> createData(String name, String category, String price,
+      String size, String color) async {
+    final db = await SQlHelper.db(); // Assuming db() returns a valid database.
+
+    final data = {
+      'name': name,
+      'category': category,
+      "price": price,
+      "size": size,
+      "color": color
+    };
+    final id = await db.insert('data', data,
+        conflictAlgorithm: sql.ConflictAlgorithm.replace);
+
+    return id;
+  }
+
+  static Future<List<Map<String, dynamic>>> getAllData() async {
+    final db = await SQlHelper.db();
+    return db.query('data', orderBy: 'id');
+  }
+
+  static Future<List<Map<String, dynamic>>> getSingleData(int id) async {
+    final db = await SQlHelper.db();
+    return db.query('data', where: "id = ?", whereArgs: [id], limit: 1);
+  }
+
+  static Future<int> updateData(int id, String name, String cat, String price,
+      String size, String color) async {
+    final db = await SQlHelper.db();
+    final data = {
+      'name': name,
+      'category': cat,
+      'price': price,
+      'size': size,
+      'color': color
+    };
+    final result =
+        await db.update('data', data, where: "id = ?", whereArgs: [id]);
+    return result;
+  }
+
+  static Future<void> deleteData(int id) async {
+    final db = await SQlHelper.db();
+    try {
+      await db.delete('data', where: "id =?", whereArgs: [id]);
+    } catch (e) {}
+  }
+}
